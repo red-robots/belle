ce56d1978290b78fdfbfe4e9c88bbbfb52286355
Fritz <fre.healy@gmail.com>
2016-08-17 15:18:55 -0400

    adding social icons and font-awesome for idx

--- template-parts/social.php
+++ template-parts/social.php
@@ -15,4 +15,10 @@
 	<?php if(get_field("instagram_link","option")):?>
 		<li class="instagram"><a href="<?php echo get_field("instagram_link","option");?>" target="_blank"><i class="fa fa-instagram"></i></a></li>
 	<?php endif;?>
+	<?php if(get_field("twitter_link","option")):?>
+		<li class="twitter"><a href="<?php echo get_field("twitter_link","option");?>" target="_blank"><i class="fa fa-twitter"></i></a></li>
+	<?php endif;?>
+	<?php if(get_field("pinterest_link","option")):?>
+		<li class="pinterest"><a href="<?php echo get_field("pinterest_link","option");?>" target="_blank"><i class="fa fa-pinterest"></i></a></li>
+	<?php endif;?>
 </ul>
